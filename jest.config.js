4c933bd476bbb538797e352a15bf2764b1ecf305
David De Vargas <dvccueter@gmail.com>
2021-11-09 16:41:59 -0500

    fix: resolving sass transform in Jest

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
+    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
     "^.+\\.svg$": "<rootDir>/.jest/svgTransform.js"
   },
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
